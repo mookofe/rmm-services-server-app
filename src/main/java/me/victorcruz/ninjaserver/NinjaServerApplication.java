6341ecd47823228aa5f0a8c10d0b306453671bc4
Victor Cruz <cruzrosario@gmail.com>
2021-01-29 20:16:47 -0500

    Initial commit

---
+++ src/main/java/me/victorcruz/ninjaserver/NinjaServerApplication.java
@@ -0,0 +1,13 @@
+package me.victorcruz.ninjaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/victorcruz/ninjaserver/NinjaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.victorcruz.ninjaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
