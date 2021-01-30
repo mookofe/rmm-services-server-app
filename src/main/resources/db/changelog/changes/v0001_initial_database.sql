d797dd3bc807ab12302ac9bf203af007c37ee094
Victor Cruz <cruzrosario@gmail.com>
2021-01-29 22:39:32 -0500

    Include table to store device services

--- src/main/resources/db/changelog/changes/v0001_initial_database.sql
+++ src/main/resources/db/changelog/changes/v0001_initial_database.sql
@@ -8,8 +8,8 @@ create table companies (
   primary key (id)
 );
 
--- Products
-create table products (
+-- Services
+create table services (
   id            VARCHAR(36) not null,
   name          VARCHAR(50) not null,
   price         DECIMAL(10,2) not null,
@@ -23,11 +23,21 @@ create table products (
 -- Devices
 create table devices (
   id            VARCHAR(36) not null,
-  company_id    VARCHAR(36) not null,
+  company_id    VARCHAR(36) not null REFERENCES companies (id),
   system_name   VARCHAR(100) not null,
   type          VARCHAR(20) not null,
   created_at    TIMESTAMP not null,
   updated_at    TIMESTAMP not null,
 
   primary key (id)
+);
+
+create table device_services (
+  id            VARCHAR(36) not null,
+  device_id     VARCHAR(36) not null REFERENCES devices (id),
+  service_id    VARCHAR(36) not null REFERENCES services (id),
+  created_at    TIMESTAMP not null,
+
+  primary key (id),
+  UNIQUE (device_id, service_id)
 );
